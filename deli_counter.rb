879a60de047d72124d687e759cfe670eaf7338a2
michael thomas <michael.thomas2014@gmail.com>
2020-05-09 02:24:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line(katz_deli)
+  count = 1
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |name|
+      puts " #{katz_deli.index(name) + 1}. #{name}"
+    end
+  end
+end
+
+#def take_a_number(katz_deli, name)
+  #puts "#{katz_deli.index(name)+1}. #{name} "
+#end
+  
