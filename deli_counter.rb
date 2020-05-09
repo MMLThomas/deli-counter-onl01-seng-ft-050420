7b3472e9eecd50b43e6f2c64d40e615091f1eef3
michael thomas <michael.thomas2014@gmail.com>
2020-05-09 02:59:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,12 @@
 # Write your code here.
 def line(katz_deli)
-  new_line = []
-  count = 1
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else
-    while count <= katz_deli.size
     katz_deli.each do |name|
       new_line << "#{katz_deli.index(name) + 1}. #{name}"
     end
-    puts "The line is currently #{new_line.join(" ")}"
+    puts "The line is currently: #{new_line.join(" ")}"
   end
 end
 
