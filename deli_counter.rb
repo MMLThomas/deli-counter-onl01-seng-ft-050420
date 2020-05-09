7d7389747535ecae2f47c3a64c630d06b70bace7
michael thomas <michael.thomas2014@gmail.com>
2020-05-09 02:27:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each do |name|
-      puts " #{katz_deli.index(name) + 1}. #{name}"
+      puts " #{katz_deli.index(name)}. #{name}"
     end
   end
 end
