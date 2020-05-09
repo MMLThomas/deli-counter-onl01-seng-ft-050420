d29c1c0c444e56285379423f7a8fa3f6e22a00c4
michael thomas <michael.thomas2014@gmail.com>
2020-05-09 02:33:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each do |name|
-      puts "Primary Color #{} is #{name} letters long."
+      puts "Primary Color #{katz_deli.index(name)} is #{name} letters long."
     end
   end
 end
