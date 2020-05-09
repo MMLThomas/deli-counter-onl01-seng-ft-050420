b35cbc68e374e0ae25f431cf1626e45a8c6f7f9c
michael thomas <michael.thomas2014@gmail.com>
2020-05-09 03:12:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,9 @@ end
   
 def now_serving(katz_deli)
   if katz_deli.size == 0
-    puts
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Now serving #{katz_deli[0]}"
+    katz_deli.shift
+  end
 end
